5acc1776dca23028507026d30964960642a4f9c2
Edzhevit <nightwalker_@abv.bg>
2020-03-13 16:13:36 +0200

    Product Manager Pages Build And Test

--- MyShop/MyShop.Core/Models/Product.cs
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    class Product
+    public class Product
     {
         public  string Id { get; set; }
 

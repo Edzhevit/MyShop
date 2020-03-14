d3f40bfe086e5cd879c275d6b6b52dee437f16c9
Edzhevit <nightwalker_@abv.bg>
2020-03-14 14:40:41 +0200

    Add entity framework

---
+++ MyShop/MyShop.DataAccess.SQL/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.SQL.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> Categories { get; set; }
+    }
+}

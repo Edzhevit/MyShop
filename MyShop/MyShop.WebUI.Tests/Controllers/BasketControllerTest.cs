7f25f39d10e19aa8514d7f7a43b69b9a72821f44
Edzhevit <nightwalker_@abv.bg>
2020-03-15 16:13:26 +0200

    Added More Tests

--- MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.Services.Services;
 using MyShop.WebUI.Controllers;
 using MyShop.WebUI.Tests.Mocks;
@@ -37,6 +39,34 @@ namespace MyShop.WebUI.Tests.Controllers
             Assert.IsNotNull(basket);
             Assert.AreEqual(1, basket.BasketItems.Count);
             Assert.AreEqual("1", basket.BasketItems.ToList().FirstOrDefault().ProductId);
+        }
+
+        [TestMethod]
+        public void CanGetSummaryViewModel()
+        {
+            //Arrange
+            IRepository<Basket> basketRepository = new MockRepository<Basket>();
+            IRepository<Product> productRepository = new MockRepository<Product>();
+
+            productRepository.Insert(new Product(){Id = "1", Price = 10.00m});
+            productRepository.Insert(new Product(){Id = "2", Price = 5.00m});
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem(){ProductId = "1", Quantity = 2});
+            basket.BasketItems.Add(new BasketItem(){ProductId = "2", Quantity = 1});
+            basketRepository.Insert(basket);
+
+            IBasketService basketService = new BasketService(productRepository, basketRepository);
+            var controller = new BasketController(basketService);
+            MockHttpContext httpContext = new MockHttpContext();
+            httpContext.Request.Cookies.Add(new HttpCookie("eCommerceBasket"){Value = basket.Id});
+            controller.ControllerContext = new ControllerContext(httpContext, new RouteData(), controller);
+
+            var result = controller.BasketSummary() as PartialViewResult;
+            var basketSummary = (BasketSummaryViewModel) result.ViewData.Model;
+
+            Assert.AreEqual(3, basketSummary.BasketCount);
+            Assert.AreEqual(25.00m, basketSummary.BasketTotal);
         }
     }
 }

f953827eabfa7b69d29905239977ad1b45c0fa5f
Edzhevit <nightwalker_@abv.bg>
2020-03-16 15:02:29 +0200

    Add Test For Orders

--- MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/BasketControllerTest.cs
@@ -22,11 +22,13 @@ namespace MyShop.WebUI.Tests.Controllers
             //Arrange
             IRepository<Basket> basketRepository = new MockRepository<Basket>();
             IRepository<Product> productRepository = new MockRepository<Product>();
+            IRepository<Order> orderRepository = new MockRepository<Order>();
             MockHttpContext httpContext = new MockHttpContext();
 
             IBasketService basketService = new BasketService(productRepository, basketRepository);
+            IOrderService orderService = new OrderService(orderRepository);
 
-            var  controller = new BasketController(basketService);
+            var  controller = new BasketController(basketService, orderService);
             controller.ControllerContext = new ControllerContext(httpContext, new RouteData(), controller);
 
             //Act
@@ -47,6 +49,7 @@ namespace MyShop.WebUI.Tests.Controllers
             //Arrange
             IRepository<Basket> basketRepository = new MockRepository<Basket>();
             IRepository<Product> productRepository = new MockRepository<Product>();
+            IRepository<Order> orderRepository = new MockRepository<Order>();
 
             productRepository.Insert(new Product(){Id = "1", Price = 10.00m});
             productRepository.Insert(new Product(){Id = "2", Price = 5.00m});
@@ -57,7 +60,8 @@ namespace MyShop.WebUI.Tests.Controllers
             basketRepository.Insert(basket);
 
             IBasketService basketService = new BasketService(productRepository, basketRepository);
-            var controller = new BasketController(basketService);
+            IOrderService orderService = new OrderService(orderRepository);
+            var controller = new BasketController(basketService, orderService);
             MockHttpContext httpContext = new MockHttpContext();
             httpContext.Request.Cookies.Add(new HttpCookie("eCommerceBasket"){Value = basket.Id});
             controller.ControllerContext = new ControllerContext(httpContext, new RouteData(), controller);
@@ -67,6 +71,45 @@ namespace MyShop.WebUI.Tests.Controllers
 
             Assert.AreEqual(3, basketSummary.BasketCount);
             Assert.AreEqual(25.00m, basketSummary.BasketTotal);
+        }
+
+        [TestMethod]
+        public void CanCheckoutAndCreateOrder()
+        {
+
+            //Arrange
+            IRepository<Product> productRepository = new MockRepository<Product>();
+            IRepository<Basket> basketRepository = new MockRepository<Basket>();
+
+            productRepository.Insert(new Product() { Id = "1", Price = 10.00m });
+            productRepository.Insert(new Product() { Id = "2", Price = 5.00m });
+
+            Basket basket = new Basket();
+            basket.BasketItems.Add(new BasketItem() { ProductId = "1", Quantity = 2, BasketId = basket.Id});
+            basket.BasketItems.Add(new BasketItem() { ProductId = "2", Quantity = 1, BasketId = basket.Id});
+
+            basketRepository.Insert(basket);
+
+            IBasketService basketService = new BasketService(productRepository, basketRepository);
+
+            IRepository<Order> orderRepository = new MockRepository<Order>();
+            IOrderService orderService = new OrderService(orderRepository);
+
+            var controller = new BasketController(basketService, orderService);
+            MockHttpContext httpContext = new MockHttpContext();
+            httpContext.Request.Cookies.Add(new HttpCookie("eCommerceBasket") { Value = basket.Id });
+            controller.ControllerContext = new ControllerContext(httpContext, new RouteData(), controller);
+
+            //Act
+            Order order = new Order();
+            controller.Checkout(order);
+
+            //Assert
+            Assert.AreEqual(2, order.OrderItems.Count);
+            Assert.AreEqual(0, basket.BasketItems.Count);
+
+            Order orderInRep = orderRepository.Find(order.Id);
+            Assert.AreEqual(2, orderInRep.OrderItems.Count);
         }
     }
 }
